e3cb03d3bf20ff79afe9804706a06f462885e40d
GitHub Action <action@github.com>
2021-02-04 23:47:45 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/yonaappletree/yonakotlinfillmethod/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.yonaappletree.yonakotlinfillmethod.services
+
+import com.github.yonaappletree.yonakotlinfillmethod.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
